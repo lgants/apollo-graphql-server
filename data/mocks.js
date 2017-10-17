20c43269fb595b6bcd2f7ca7c887813bf203acf7
lgants <lgants@gmail.com>
2017-10-16 19:46:40 -0500

    completed app

--- data/mocks.js
+++ data/mocks.js
@@ -1,5 +1,15 @@
+import casual from 'casual';
+
+// at a high level, mocks for each type just define a function that gets run when an object of that type should be returned
 const mocks = {
   String: () => 'It works!',
+  Query: () => ({
+    author: (root, args) => {
+      return { firstName: args.firstName, lastName: args.lastName };
+    },
+  }),
+  Author: () => ({ firstName: () => casual.first_name, lastName: () => casual.last_name }),
+  Post: () => ({ title: casual.title, text: casual.sentences(3) }),
 };
 
 export default mocks;
--- data/schema.js
+++ data/schema.js
@@ -4,14 +4,32 @@ import {
 } from 'graphql-tools';
 import mocks from './mocks'
 
+import resolvers from './resolvers';
+
 const typeDefs = `
+type Author {
+  id: Int
+  firstName: String
+  lastName: String
+  posts: [Post]
+}
+
+type Post {
+  id: Int
+  title: String
+  text: String
+  views: Int
+  author: Author
+}
+
 type Query {
-  testString: String
+ author(firstName: String, lastName: String): Author
+ getFortuneCookie: String
 }
 `;
 
-const schema = makeExecutableSchema({ typeDefs });
+// const schema = makeExecutableSchema({ typeDefs });
+// addMockFunctionsToSchema({ schema, mocks });
+// export default schema;
 
-addMockFunctionsToSchema({ schema, mocks });
-
-export default schema;
+export default makeExecutableSchema({ typeDefs, resolvers })
